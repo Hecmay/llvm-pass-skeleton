337923a29b874c530be6ee29c24f0878d9b7b838
Hecmay <sx233@cornell.edu>
2019-11-11 17:38:26 -0500

    [add] indvar canonicalization

--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -1,5 +1,6 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
+#include "llvm/Analysis/LoopPass.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/IR/InstrTypes.h"
@@ -7,11 +8,18 @@
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/Analysis/TargetLibraryInfo.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/Dominators.h"
 #include "llvm/Analysis/LoopInfo.h" 
 #include "llvm/IR/Dominators.h"
+#include "llvm/Transforms/Scalar.h"
 
+#include "llvm/ADT/SmallPtrSet.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/Analysis/AssumptionCache.h"
+#include "llvm/Analysis/ScalarEvolution.h"
+#include "llvm/Analysis/TargetLibraryInfo.h"
 #include <assert.h>
 using namespace llvm;
 
@@ -23,62 +31,75 @@ namespace {
     void getAnalysisUsage(AnalysisUsage &AU) const {
       AU.setPreservesCFG();
       AU.addRequired<LoopInfoWrapperPass>();
+      AU.addRequired<TargetLibraryInfoWrapperPass>();
     }
 
     virtual bool runOnFunction(Function &F) {
+
       // craete llvm function with
       LLVMContext &Ctx = F.getContext();
       std::vector<Type*> paramTypes = {Type::getInt32Ty(Ctx)};
       Type *retType = Type::getVoidTy(Ctx);
       FunctionType *logFuncType = FunctionType::get(retType, paramTypes, false);
-      // get module out of function  
       Module* module = F.getParent();
       Constant *logFunc = module->getOrInsertFunction("logop", logFuncType);
 
-      // analyze the loops 
-      bool mutate = false;
+      // perform constant prop and loop analysis
+      auto constProp = createConstantPropagationPass();
+      bool changed = constProp->runOnFunction(F);
       LoopInfo &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
-      DominatorTree DT(F); LI.analyze(DT);
 
-      // process each loop
-      for (auto &B : F) {
-        BasicBlock* b = &B;
-        auto loop = LI.getLoopFor(b);
-        if (L.contain(B)) void;
-        if (loop != nullptr) {
-          auto k = loop->getHeader()->getName();
-          errs() << k << ":" << B.getName() << "\n";
-        }
-        // errs() << "analyze bb: " << B.getName() << "\n";
-        // analyze basic blocks with loops
-        if (auto bi = dyn_cast<BranchInst>(B.getTerminator())) {
-          // Value *loopCond = bi->getCondition();
-          for (auto &I : B) {
-            if(isa<CallInst>(&I) || isa<InvokeInst>(&I)){
-              errs() << cast<CallInst>(&I)->getCalledFunction()->getName() << "\t";
-            }
-            if (auto *op = dyn_cast<BinaryOperator>(&I)) {
+      // induction var canonicalization  
+      PassManagerBuilder builder;
+      builder.OptLevel = 0;
+      legacy::FunctionPassManager FPM(module);
+      FPM.add(createIndVarSimplifyPass());
+      FPM.run(F);
 
-              IRBuilder<> builder(op); // ir builder at op
-              Value *lhs = op->getOperand(0);
-              Value *rhs = op->getOperand(1);
-              Value *mul = builder.CreateMul(lhs, rhs);
+      // induction var reduction
+      for(auto* L : LI){
+        if (PHINode *Node = L->getCanonicalInductionVariable()) {
+          errs() << Node->getName() <<"\n";
+          // auto workList = L->getBlocksSet();
+          // SmallVector<Instruction *, 16> WorkListVec;
+          // for (Instruction &I : instructions(*loop)) {
+          //   WorkListVec.push_back(&I);
+          // }
+        }        
+
+        bool mutate = false;
+        for (auto &B : F) {
+          BasicBlock* b = &B;
+          // errs() << "analyze bb: " << B.getName() << "\n";
+          // analyze basic blocks with loops
+          if (auto bi = dyn_cast<BranchInst>(B.getTerminator())) {
+            // Value *loopCond = bi->getCondition();
+            for (auto &I : B) {
+              if(isa<CallInst>(&I) || isa<InvokeInst>(&I)){
+                errs() << cast<CallInst>(&I)->getCalledFunction()->getName() << "\n";
+              }
+              if (auto *op = dyn_cast<BinaryOperator>(&I)) {
+
+                IRBuilder<> builder(op); // ir builder at op
+                Value *lhs = op->getOperand(0);
+                Value *rhs = op->getOperand(1);
+                Value *mul = builder.CreateMul(lhs, rhs);
   
-              mutate = true;
-              for (auto &U : op->uses()) {
-                User *user = U.getUser();  
-                // user->setOperand(U.getOperandNo(), mul);
+                mutate = true;
+                for (auto &U : op->uses()) {
+                  User *user = U.getUser();  
+                  // user->setOperand(U.getOperandNo(), mul);
+                }
               }
             }
           }
         }
       }
-
-    // dump the mutated llvm ir & execute 
-    std::error_code ecode;
-    raw_fd_ostream dest("opt.ll", ecode);
-    module->print(dest, nullptr);
-    return true;
+      // dump the mutated llvm ir & execute 
+      std::error_code ecode;
+      raw_fd_ostream dest("opt.ll", ecode);
+      module->print(dest, nullptr);
+      return true;
     }
   };
 }
