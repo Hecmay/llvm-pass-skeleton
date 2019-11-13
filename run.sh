8a4e9cc4432558a22964cfee591cb26be0ce9cde
yz882 <yz882@cornell.edu>
2019-11-12 23:35:23 -0500

    Modify bash script to run embench.

--- run.sh
+++ run.sh
@@ -1,13 +1,28 @@
+set -x
+
 # setup env variable 
 export PATH="/work/zhang-x1/common/install/llvm-8.0/build/bin/":$PATH
 export LLVM_DIR=/work/zhang-x1/common/install/llvm-8.0/build/lib/cmake/llvm/
+export EMBENCH_DIR=/work/zhang-x1/users/yz882/cs6120/llvm-pass-skeleton/embench-iot/
 # compile pass to generate shared lib
-rm -rf build; mkdir build; cd build/; cmake ../; make; cd -
+#rm -rf build; mkdir build; cd build/; cmake ../; make; cd -
 # compile to llvm bitcode with clang fromtemd
-clang -c -emit-llvm -O0 -Xclang -disable-O0-optnone example.c -o original.bc
+clang -c -emit-llvm -O0 -Xclang -disable-O0-optnone $1/*.c $EMBENCH_DIR/support/*.c -I$1 \
+-I$EMBENCH_DIR/support -DCPU_MHZ=1024
+
 # generate llvm ir from bitcode
-llvm-dis original.bc 
-# optimze with llvm opt
-opt -S -load build/skeleton/libSkeletonPass.so -mem2reg -sr original.ll -o opt.ll
-opt -S -load build/skeleton/libSkeletonPass.so -mem2reg -sr -dce original.ll -o opt.bc
-llc -filetype=obj opt.bc; gcc opt.o; ./a.out
+for f in *.bc
+do
+  llvm-dis ${f} 
+done
+
+for f in *.ll
+do
+  # optimze with llvm opt
+  opt -S -load build/skeleton/libSkeletonPass.so -mem2reg -sr ${f} -o opt_${f}
+  opt -S -load build/skeleton/libSkeletonPass.so -mem2reg -sr -dce ${f} -o opt_${f}.bc
+  llc -filetype=obj opt_${f}.bc; 
+done
+
+gcc *.o; 
+./a.out
